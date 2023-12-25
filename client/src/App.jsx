ce77e40927a6206b62fddb0a816c38a55552457d
Ashish <ashish.gaurav2003@gmail.com>
2023-12-25 19:14:12 +0530

    first pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import SignIn from './pages/signin';
+import SignUp from './pages/signup';
+import Profile from './pages/profile';
 
 export default function App() {
   return (
-    <h1 className="text-red-500">App</h1>
+    <BrowserRouter> 
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
